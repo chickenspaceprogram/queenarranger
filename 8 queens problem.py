89b5a6fc8b332bf007f5d6352dabf68db6cd156e
chickenspaceprogram <pestpestthechicken@yahoo.com>
2024-03-16 15:27:17 -0700

    Solver can now solve boards of any size, not just 8x8.

--- 8 queens problem.py
+++ 8 queens problem.py
@@ -4,7 +4,8 @@ def board_valid(row: int, col: int):
     Returns whether the board inputted is valid if a queen is added to the space given by row, col. 
     '''
     global board
-    if (1 in board[row]) or (1 in board[:, col]) or (1 in numpy.diagonal(board, offset=(col - row))) or (1 in numpy.rot90(board).diagonal(offset=(row + col - 7))):
+    global board_size
+    if (1 in board[row]) or (1 in board[:, col]) or (1 in numpy.diagonal(board, offset=(col - row))) or (1 in numpy.rot90(board).diagonal(offset=(row + col - board_size + 1))):
         return False
     return True
 
@@ -15,18 +16,19 @@ def print_board(board: numpy.ndarray):
     global num_boards
     num_boards += 1
     boardaslist = board.tolist()
+    global board_size
     print()
-    print(f"Board no. {num_boards}")
-    for row in range(8):
+    print(f"Board {num_boards}")
+    for row in range(board_size):
         disp_string = ''
-        print('|' + '---|' * 8)
-        for col in range(8):
+        print('|' + '---|' * board_size)
+        for col in range(board_size):
             if boardaslist[row][col] == 1:
                 disp_string = f'{disp_string}| Q '
             else:
                 disp_string = f'{disp_string}|   '
         print(disp_string + '|')
-    print('|' + '---|' * 8)
+    print('|' + '---|' * board_size)
 
 def is_new_board(board: numpy.ndarray):
     '''
@@ -43,12 +45,6 @@ def is_new_board(board: numpy.ndarray):
                 return False
     return True
 
-
-board = numpy.zeros((8, 8))
-num_queens = 0
-num_boards = 0
-boards_list = []
-
 def recursive_solver(initrow: int):
     '''
     A recursive function that solves stuff using backtracking.
@@ -57,18 +53,28 @@ def recursive_solver(initrow: int):
     global num_queens
     global num_boards
     global boards_list
-    for col in range(8):
+    global board_size
+    for col in range(board_size):
         if board_valid(initrow, col):
             board[initrow, col] = 1
-            if initrow >= 7:
+            if initrow >= (board_size - 1):
                 boards_list.append(board.copy())
                 board[initrow, col] = 0
                 return
             recursive_solver(initrow + 1)
             board[initrow, col] = 0
 
+num_queens = 0
+num_boards = 0
+boards_list = []
+board_size = int(input("Please enter desired board size: "))
+board = numpy.zeros((board_size, board_size))
 recursive_solver(0)
+
 lenboardslist = len(boards_list)
+
+if lenboardslist == 0:
+    print("No boards found.")
 
 for i in range(lenboardslist):
     board_check = boards_list.pop(0)
